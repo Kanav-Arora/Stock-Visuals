cfcaa89a5cb988e6b5695b92b1f8c12ea8444f3a
Kanav Arora <kanavarora1515@gmail.com>
2021-09-15 10:40:57 +0530

    API Exposure layered

--- backend.py
+++ backend.py
@@ -1,9 +1,11 @@
 from urllib.request import urlopen
 import json
+import test
 
 def daily_o_c(name, date):
-    url = "https://api.polygon.io/v1/open-close/"+name+"/"+ str(date)+"?adjusted=true&apiKey=llQvmrJ_Cr2t5iPAii02EgeiD2hY8Fr2"
-    # url = "https://api.polygon.io/v1/open-close/AAPL/2020-10-14?adjusted=true&apiKey=llQvmrJ_Cr2t5iPAii02EgeiD2hY8Fr2"
+    url = test.daily_o_c_api(name,date)
+    # remove above line and add
+    # url = "https://api.polygon.io/v1/open-close/"+name+"/"+ str(date)+"?adjusted=true&apiKey=<your_api_key>"
     response = urlopen(url)
     data_json = json.loads(response.read())
     return data_json
