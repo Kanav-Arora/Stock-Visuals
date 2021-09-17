85a117d702d7833f89df76c42a9bf92e1e3dd1d6
Kanav Arora <kanavarora1515@gmail.com>
2021-09-17 23:14:20 +0530

    Crypto Test API added

--- backend.py
+++ backend.py
@@ -40,3 +40,11 @@ def refresh():
 """ ----------------------------------------------------------------------------------------------------------------------------------"""
 
 
+def crypto_daily_o_c(name, date):
+    url = test.daily_o_c_api(name,date)
+    # remove above line and add
+    # url = "https://api.polygon.io/v1/open-close/"+name+"/"+ str(date)+"?adjusted=true&apiKey=<YOUR_API_KEY>"
+    response = urlopen(url)
+    data_json = json.loads(response.read())
+    return data_json
+
