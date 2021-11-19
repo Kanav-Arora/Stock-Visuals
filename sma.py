64fbd3be3c97e253fbc7d6ee9a8a32a2c4cc0f61
Shivi2401 <shivamb2401@gmail.com>
2021-11-19 18:11:37 +0530

    Moving average indicator

--- sma.py
+++ sma.py
@@ -6,13 +6,13 @@ NavigationToolbar2Tk)
 from matplotlib.figure import Figure
 import pandas as pd
 import tkinter as tk
-import ttk
+from tkinter import ttk
 
 root = tk.Tk()
 
 
 dlf = ttk.Labelframe(root, text='Plot Area')
-lf.grid(row=0, column=0, sticky='nwes', padx=3, pady=3)
+dlf.grid(row=0, column=0, sticky='nwes', padx=3, pady=3)
 
 t = np.arange(0.0,3.0,0.01)
 df = pd.DataFrame({'t':t, 's':np.sin(2*np.pi*t)})
@@ -22,16 +22,11 @@ ax = fig.add_subplot(111)
 
 df.plot(x='t', y='s', ax=ax)
 
-canvas = FigureCanvasTkAgg(fig, master=lf)
-canvas.show()
-canvas.get_tk_widget().grid(row=0, column=0)
-
-root.mainloop()
 
 # start_date = '2015-01-01'
 # end_date = '2016-12-31'
 
-# fig, ax = plt.subplots(figsize=(16,9))
+# fig, ax = plot.subplots(figsize=(16,9))
 
 # ax.plot(data.loc[start_date:end_date, :].index, data.loc[start_date:end_date, 'MSFT'], label='Price')
 # ax.plot(long_rolling.loc[start_date:end_date, :].index, long_rolling.loc[start_date:end_date, 'MSFT'], label = '100-days SMA')
@@ -40,4 +35,11 @@ root.mainloop()
 # ax.legend(loc='best')
 # ax.set_ylabel('Price in $')
 # ax.xaxis.set_major_formatter(my_year_month_fmt)
+
+
+canvas = FigureCanvasTkAgg(fig, master=dlf)
+canvas.show()
+canvas.get_tk_widget().grid(row=0, column=0)
+
+root.mainloop()
 
